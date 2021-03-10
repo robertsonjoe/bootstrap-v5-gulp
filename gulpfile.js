31ad3847b8bbff9c0419fe0646a79d39c9c68e36
Joe Robertson <jwj.robert@gmail.com>
2021-03-10 10:53:39 -0500

    Add base gulp file

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+gulp.task('sass', () => {
+  return gulp.src('./sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('start', gulp.series('sass', () => {
+  browserSync.init({
+    server: './',
+  });
+
+  gulp.watch('sass/*.scss', gulp.series('sass'));
+  gulp.watch("./*.html").on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.series('start'));

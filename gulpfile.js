303c85fddfa3e6814d2938503e8102912027f75e
Joe Robertson <jwj.robert@gmail.com>
2021-03-10 11:06:46 -0500

    Add sourcemaps and clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', () => {
   return gulp.src('./sass/*.scss')
     .pipe(sass())
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist/'))
     .pipe(browserSync.stream());
 });
